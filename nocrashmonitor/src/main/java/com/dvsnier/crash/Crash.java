4deaed4f67f832b665d01a536d66ea316496e5c4
lizhenwei <dovsnier@qq.com>
2018-06-28 13:34:12 +0800

    nocrash assembly optimization

---
+++ nocrashmonitor/src/main/java/com/dvsnier/crash/Crash.java
@@ -0,0 +1,60 @@
+package com.dvsnier.crash;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.dvsnier.monitor.common.ITag;
+
+
+/**
+ * crash monitoring
+ * Created by dovsnier on 2018/6/27.
+ */
+public final class Crash implements ITag {
+
+    protected static Context context;
+
+    /**
+     * initializing crash monitoring
+     *
+     * @param context {@see Context}
+     */
+    public static void initialize(@NonNull Context context) {
+        Crash.context = context;
+        // no development phase log records are provided in release mode
+    }
+
+    /**
+     * initializing crash monitoring
+     *
+     * @param context {@see Context}
+     * @param mode    true: the debug mode,otherwise release mode
+     */
+    public static void initialize(@NonNull Context context, boolean mode) {
+        Crash.context = context;
+        if (mode)
+            Log.d(TAG, "no development phase log records are provided in release mode");
+    }
+
+    /**
+     * initializing crash monitoring
+     *
+     * @param context {@see Context}
+     * @param mode    true: the debug mode,otherwise release mode
+     * @param tips    the standard abnormal text hints
+     */
+    public static void initialize(@NonNull Context context, boolean mode, String tips) {
+        Crash.context = context;
+        if (mode)
+            Log.d(TAG, "no development phase log records are provided in release mode");
+    }
+
+    /**
+     * to closed server monitor
+     */
+    public static void shutdown() {
+        context = null;
+        // no development phase log records are provided in release mode
+    }
+}
---
+++ nocrashmonitor/src/main/java/com/dvsnier/monitor/common/ITag.java
@@ -0,0 +1,10 @@
+package com.dvsnier.monitor.common;
+
+/**
+ * log label
+ * Created by dovsnier on 2014/3/15.
+ */
+public interface ITag {
+
+    String TAG = "Crash";
+}
