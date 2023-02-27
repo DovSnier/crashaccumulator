f47cb201e2a37be026a6b7c04640bd0d613cba96
zhenwei <zhenwei.mr@qq.com>
2023-02-27 17:57:04 +0800

    com.android.support -> AndroidX

--- crash-no/src/main/java/com/dvsnier/crash/Crash.java
+++ crash-no/src/main/java/com/dvsnier/crash/Crash.java
@@ -1,7 +1,7 @@
 package com.dvsnier.crash;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 
 import com.dvsnier.monitor.common.ITag;
